f9313a152fb20b8a674a486aa52a3b3a58abb6c3
Yanoka <yanoka23@gmail.com>
2021-01-25 21:10:16 +0000

    Assignment 2-2

---
+++ Assignment 2-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  char c;
+  c = 89;
+  cout << c ;
+
+	c = 65;
+	cout << c ;
+
+	c = 78;
+	cout << c ;
+
+  c = 65;
+  cout << c << "\n";
+}
