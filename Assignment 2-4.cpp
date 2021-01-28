958e71d28d6c4a65b7b50495773c4dfa27cf7865
Yanoka <yanoka23@gmail.com>
2021-01-28 02:40:06 +0000

    fixed Assignment 2-4

--- Assignment 2-4.cpp
+++ Assignment 2-4.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main ()
 {
-  double regular_price = 5.95;
+  double regular_price = 59.95;
   double discount_amount = regular_price * 0.2;
-  double seles_price = regular_price - discount_amount;
+  double sales_price = regular_price - discount_amount;
 
-  count <<  cout << "Regular price: $" << regular_price << endl;
+  cout << "Regular price: $" << regular_price << endl;
   cout << "Discount amount: $" << discount_amount << endl;
   cout << "Sale price: $" << sales_price << endl; 
   }
