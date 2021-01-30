716a95dfd5b55c80e4e8c11b6764408361bacf79
Yanoka <yanoka23@gmail.com>
2021-01-30 04:33:43 +0000

    Assignment 2-6 done

--- Assignment 2-6.cpp
+++ Assignment 2-6.cpp
@@ -1,9 +1,31 @@
-//28, 32, 37, 24, and 33.
+//Yana Berezina
+// Write a program that  stores the following values in five different variables:
+
+// 28, 32, 37, 24, and 33.
+
+// The program should first calculate the sum of these five variables and store the result in a separate variable named sum .
+// Then, the program should divide the sum variable by 5 to get the average.
+// Display the sum and average with precision 3 on the screen.
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
+  int var1 = 28;
+  int var2 = 32;
+  int var3 = 37;
+  int ver4 = 24;
+  int ver5 = 33;
 
+  // set precision to 3 digits after decimal point
+  cout << setprecision(3);
+  cout << fixed << showpoint;
+
+  double sum = var1 + var2 + var3 + ver4 + ver5; //sum of 5 variabls
+  double average = sum / 5;
+
+  cout << "Sum: " << sum << endl;
+  cout << "Average: " << average << endl;
 }
