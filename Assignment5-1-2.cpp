8a050df5b80d757a24c2a917088e236ca971154f
Yanoka <yanoka23@gmail.com>
2021-03-03 08:04:46 +0000

    fixed loop condition

--- Assignment5-1-2.cpp
+++ Assignment5-1-2.cpp
@@ -10,22 +10,44 @@ int main()
   int nthPower=1;
   
   do{
-    cout << "Please, enter the range:" << endl;
+    cout << "Please, enter the range from 0 to 1300:" << endl;
     cin >> begin >> end;
   }
   while ((begin<0)||(end>1300));
 
-  // while ((begin < pow(2,i)) && (pow(2, i) < end)) {
-  //   cout << pow(2, i) << " ";
-  //   i++;
-  // }
+  /*
+   With pow()
+    // while ((begin < pow(2,i)) && (pow(2, i) < end)) {
+    //   cout << pow(2, i) << " ";
+    //   i++;
+    // }
+  */
 
-  while ((begin < nthPower) && (nthPower < end)) {
-    cout << nthPower << " ";
+  /*
+    Errors: because of condition on line 27, all tests failed.
+    I changed the condition to condition on line 34 to fix tests.
+
+    // while ((begin < nthPower) && (nthPower < end)) {
+    //   cout << nthPower << " ";
+    //   nthPower = nthPower * 2;
+    //   i++;
+    // }
+  */
+
+  while (nthPower < end) {
+    // Print only if data in range
+    if (nthPower > begin) {
+      cout << nthPower << " ";      
+    }
+
+    // calculate power
     nthPower = nthPower * 2;
+    
+    // change i to understand if we need to print "Not found"
     i++;
   }
 
+  // Print "not found if no data in range
   if (i == 0) 
     cout << "Not found" << endl;
 
