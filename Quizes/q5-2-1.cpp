6deac1157ce96158c61b527fd4df0a22eb7ca23a
Yanoka <yanoka23@gmail.com>
2021-03-10 16:31:29 +0000

    q5-3

---
+++ Quizes/q5-2-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+  int num,minAmount;
+  int minAmount=-1;
+  
+  srand (time(0));
+  for (int i=0;i<num;i++){
+    num = rand()%100;
+  }
+  
+
+
+
+
+}
---
+++ Quizes/q5-2-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int begin,end,i,j;
+  
+  do{
+    cout << "Please,enter two integers from 1 to 100 " << endl;
+    cin >> begin >> end; 
+  }
+  while(begin > end);
+
+  for (i=begin; i<end; i++){
+    for(j=2; j<i;j++){
+      if(i%j)
+        break;
+    
+    }
+    if (i==j)
+     cout << i << " is prime number" <<endl;
+  }
+}
