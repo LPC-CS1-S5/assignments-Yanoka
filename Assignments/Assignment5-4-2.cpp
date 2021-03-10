14ca85eb69d3c4f38d4a5267a14eec3351ca7c5f
Yanoka <yanoka23@gmail.com>
2021-03-10 08:31:59 +0000

    A5-4-2

---
+++ Assignments/Assignment5-4-2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+  ifstream ifs;
+  int num;
+  int sum = 0;
+  int numItems =0;
+  int max = -1;
+  int min = 101;
+
+  ifs.open("Numbers.txt");
+  if (ifs.fail()){
+    cerr << "File open error "<<endl;
+    exit(-1);
+  }
+
+  while (ifs)
+    {
+      ifs >> num;
+      sum+=num;
+      numItems++;
+      cout << num <<endl;
+
+      if(num>max)
+        max=num;
+      if(num<min)
+        min=num;
+
+    }
+
+  cout << numItems << endl;
+  cout << "Sum: " << sum <<endl; 
+  cout << "Min: " << min << " Max: " << max <<endl;
+  cout << "Average: " << sum/(float)numItems << endl;
+  
+  ifs.close();
+}
