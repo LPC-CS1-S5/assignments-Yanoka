080d29d4f6ac85a17127f057ab898354431aa2f6
Yanoka <yanoka23@gmail.com>
2021-03-10 17:01:03 +0000

    q5-2-1

--- Quizes/q5-2-1.cpp
+++ Quizes/q5-2-1.cpp
@@ -5,15 +5,16 @@ using namespace std;
 int main()
 {
   int num,minAmount;
-  int minAmount=-1;
+  int minAmount = 101;
   
   srand (time(0));
-  for (int i=0;i<num;i++){
+  for (int num=0;num<3 ;num++){
     num = rand()%100;
+    cout << num << endl; 
+
   }
-  
-
-
-
+  if (num < minAmount)
+    minAmount = num;
+    cout << minAmount <<endl;
 
 }
---
+++ Quizes/q5-2-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+  int M,N,i;
+  i=0
+  int nthPower=1;
