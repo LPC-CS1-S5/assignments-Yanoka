71ea48817515585d0eed0b7b0fb01ced772d01e6
Yanoka <yanoka23@gmail.com>
2021-03-10 17:31:24 +0000

    5-2-2

--- Quizes/q5-2-2.cpp
+++ Quizes/q5-2-2.cpp
@@ -4,7 +4,22 @@ using namespace std;
 
 int main()
 {
-
   int M,N,i;
-  i=0
+  i=0;
   int nthPower=1;
+
+  do{
+    cout <<  "please , enter the base number and exponent: " << endl;
+    cin >> N >> M;
+
+    cout << "power of 0: " << 1 << endl;
+
+    for (i = 1; i<M; i++) {      
+      nthPower *= N;
+      cout << "power of " << i << ": " << nthPower << endl;
+    }
+  }
+  while ();
+  //if (i == 0) 
+  //  cout << "Not found" << endl;
+}
