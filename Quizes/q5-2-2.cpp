997735f9a8e0353398fc5f8b1a2025684d3442c5
Yanoka <yanoka23@gmail.com>
2021-03-10 17:34:40 +0000

    q5-2-3

--- Quizes/q5-2-2.cpp
+++ Quizes/q5-2-2.cpp
@@ -11,15 +11,16 @@ int main()
   do{
     cout <<  "please , enter the base number and exponent: " << endl;
     cin >> N >> M;
-
-    cout << "power of 0: " << 1 << endl;
-
-    for (i = 1; i<M; i++) {      
-      nthPower *= N;
-      cout << "power of " << i << ": " << nthPower << endl;
-    }
   }
-  while ();
+  while (N <= 0 || N >= 10 || M < 1 || M > 10);
+
+  cout << "power of 0: " << 1 << endl;
+
+  for (i = 1; i<M; i++) {      
+    nthPower *= N;
+    cout << "power of " << i << ": " << nthPower << endl;
+  }
+  
   //if (i == 0) 
   //  cout << "Not found" << endl;
 }
