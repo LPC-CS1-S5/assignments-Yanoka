1fedc1de441fbb8289875df687bae49b67aa0142
Yanoka <yanoka23@gmail.com>
2021-03-31 17:50:59 +0000

    Q4

--- Midterm/Q3/isGreater.cpp
+++ Midterm/Q3/isGreater.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int isGreater(int n) {
-	
+	static int prevNumber = -1;
+
+	if(prevNumber == -1)
+	{
+		prevNumber = n;
+		return 0;
+	}
+	else
+	{
+		if(n > prevNumber)
+		{
+			prevNumber = n;
+      return 1;
+    }
+    else
+    {
+      prevNumber = n;
+      return 0;
+		}
+	}
 }
---
+++ Midterm/Q4/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <time.h>
+#include <fstream>
+
+using namespace std;
+
+int getRdnum()
+int getRdnum(int n1);
+int getRdnum(int n1,int n2);
+
+int main()
+{
+  srand(time(0));
+	cout << << endl;
+	cout << << endl;
+	cout << << endl;
+
+}
+
+int getRdnum()
+{
+    const int MAX = 100;
+    const int MIN = 1;
+    int num = rand() % (MAX - MIN) + MIN;
+    return num;
+}
+
+int getRdnum(int n1)
+{
+    const int MIN = 1;
+    int num = rand() % (n1 - MIN) + n1;
+    return num ;
+}
+
+int getRdnum(int n1,int n2)
