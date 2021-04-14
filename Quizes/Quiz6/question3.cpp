99b71068e1627222f942dd8f3d44a91b76d5a477
Yanoka <>
2021-04-14 04:34:45 +0000

    q2

---
+++ Quizes/Quiz6/question3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void gerRandnum(int &n1, int &n2, int &n3);
+int  findMin(int n1, int n2, int n3);
+
+int main()
+{
+int n1, n2;
+float f1,f2;
+}
+
+int  findMin(int n1, int n2, int n3) 
