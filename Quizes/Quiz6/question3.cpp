a2dc1808a694ce1e9256d496df97af3e512478e0
Yanoka <>
2021-04-14 04:44:47 +0000

    q2

--- Quizes/Quiz6/question3.cpp
+++ Quizes/Quiz6/question3.cpp
@@ -2,13 +2,28 @@
 
 using namespace std;
 
-void gerRandnum(int &n1, int &n2, int &n3);
+void getRandnum(int &n1, int &n2, int &n3);
 int  findMin(int n1, int n2, int n3);
+void printResult(int n1, int n2, int n3, int min);
 
 int main()
 {
-int n1, n2;
-float f1,f2;
+int min, n1, n2, n3;
+
+
+}
+
+void getRandnum(int &n1, int &n2, int &n3)
+{
+
 }
 
 int  findMin(int n1, int n2, int n3) 
+{
+
+}
+
+void printResult(int n1, int n2, int n3, int min)
+{
+
+}
