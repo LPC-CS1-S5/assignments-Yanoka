5754d7925086fea9a3f24574ae00b79bead9a228
Yanoka <>
2021-04-14 04:05:00 +0000

    Q1

---
+++ Quizes/Quiz6/question1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void swapTwoValues(int &n1, int &n2);
+void swapTwoValues(float &f1, float &f2);
+
+int main()
+{
+int n1, n2;
+float f1,f2;
+
+cout << "Please, enter the two integer numbers: " << endl;
+cin >> n1 >> n2;
+cout << "Please, enter the two float numbers: " << endl;
+cin >> f1 >> f2;
+
+swapTwoValues(n1,n2);
+swapTwoValues(f1,f2);
+
+cout << n1 << " " << n2 << endl;
+cout << f1 << " " << f2 << endl;
+
+}
+
+void swapTwoValues(int &n1, int &n2);
+
+
+void swapTwoValues(float &f1, float &f2);
