f5f81c371d2d4dbebe1754a8548aac57624047d9
Yanoka <>
2021-04-14 05:01:46 +0000

    q3

--- Quizes/Quiz6/question3.cpp
+++ Quizes/Quiz6/question3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -10,17 +11,27 @@ int main()
 {
 int min, n1, n2, n3;
 
+srand(time(0));
 
 }
 
 void getRandnum(int &n1, int &n2, int &n3)
 {
-
+	n1 = rand() % 100;  
+	n2 = rand() % 100;
+	n3 = rand() % 100;
 }
 
 int  findMin(int n1, int n2, int n3) 
 {
+	int min = n1;
 
+	if ( min > n2)
+		min = n2;
+	if ( min > n3)
+		min = n3;
+
+return min;
 }
 
 void printResult(int n1, int n2, int n3, int min)
