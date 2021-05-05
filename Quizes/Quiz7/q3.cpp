419c5d36cb83f24080e67d46a7a4511aae491fd4
Yanoka <>
2021-05-05 04:43:05 +0000

    q3

--- Quizes/Quiz7/q3.cpp
+++ Quizes/Quiz7/q3.cpp
@@ -7,10 +7,14 @@ int main()
 {
     const int N = 10;
     int numbers[N] = {10,15,0,-25,19,20,25,30,45,20};
-    
-    printtriangle(numbers, SIZE);
-}
-void findMax(int num[], int, int)
-{
+		int from = 0;
+		int to = 5;
 
+    findMax( num, from, to );
+		findMax( num, from+5, to+5 );
+}
+void findMax(int num[], int from, int to)
+{
+	for (int i=from; i < to; i++)
+		if (max < ) 
 }
