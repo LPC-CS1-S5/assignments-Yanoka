268e4b28e6afd357b776fc4d8ca69d500e887d92
Yanoka <>
2021-05-12 05:24:02 +0000

    q2

--- Quizes/Quiz8/q2.cpp
+++ Quizes/Quiz8/q2.cpp
@@ -18,3 +18,45 @@ int 	main()
 		if( domaincheck(email))
 			cout << "Domain check passed\n";
 }
+bool 	idcheck(char email[])
+{
+	// Id len
+	int maxemaillen = 15;
+	int minemaillen = 5;
+
+	int i = 0;
+	// Start with alphabet character
+	if (!isalpha(email[0]))
+		return false;
+
+	while (email[i] != '@') {
+		i++;
+	}
+
+	if (i<= maxemaillen && i >= minemaillen)
+		return true;
+	else
+		return false;
+}
+bool	domaincheck(char email[])
+{
+	// The domain name should be one of the list {com, org, edu, net, and gov}
+	char domeinar[] = "com org edu net gov";
+	char *result;
+	int len, i;
+
+	len = strlen(email);
+	i = len-1;
+	
+	do {
+		if(email[i] == '.')
+		break;
+	}
+	while(i-- > 0);
+
+	result = strstr( domeinar, email+i+1 );
+	if (result)
+		return true;
+	else
+		return false;
+}
