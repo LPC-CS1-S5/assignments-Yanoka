bb6b00a7fa36e2c00ce4584106bdf833eb9234ca
Yanoka <>
2021-05-26 04:32:00 +0000

    q3

--- Final/q3.cpp
+++ Final/q3.cpp
@@ -2,21 +2,22 @@
 
 using namespace std;
 
-bool subset();
+bool subset(int[], int[]);
 
 const int SIZE1 = 5;
 const int SIZE2 = 10;
-int numbers1[SIZE1] = {2, 3, 5, 9, 10};
-int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
 
 int main() {
-	if (subset())
+	int numbers1[SIZE1] = {2, 3, 5, 9, 10};
+	int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
+
+	if (subset(numbers1, numbers2))
 		cout << "Subset" << endl;
 	else
 		cout << "Not subset" << endl;
 }
 
-bool subset(){
+bool subset(int numbers1[], int numbers2[]){
 	for (int i = 0; i < SIZE2; i++) {
 		for (int j = 0; j < SIZE1; j++) {
 			if (numbers1[j] != numbers2[i+j]) {
