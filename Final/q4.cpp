0354037ee34c0f6e10e609ca14932cea8f647a90
Yanoka <>
2021-05-26 04:59:09 +0000

    q4

--- Final/q4.cpp
+++ Final/q4.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 void subset(string[], string[]);
 
+int splitwords(string txt, char delimiter, string userinput);
+
 const int SIZE1 = 5;
 const int SIZE2 = 10;
 
@@ -16,22 +18,49 @@ int main() {
   cout << "Enter user string "; 
   cin >> userinput;
 
-	if (subset(words, userinput))
-		cout << "Subset" << endl;
-	else
-		cout << "Not subset" << endl;
+	splitwords(words, ' ');
+
+	// if (subset(words, userinput))
+	// 	cout << "Subset" << endl;
+	// else
+	// 	cout << "Not subset" << endl;
 }
 
-void subset(string words[], string userinput[]){
-	for (int i = 0; i < SIZE2; i++) {
-		for (int j = 0; j < SIZE1; j++) {
-			if (numbers1[j] != numbers2[i+j]) {
-				break;
-			}
+int splitwords(string txt, char delimiter, string userinput)
+{
+	int start=0;
+	int cnt=0;
+	size_t sub_string_found;
+	// string::iterator found;
+	size_t found;
+	string split;
 
-			if (j == SIZE1 - 1)
-				return true;
+	while ((found = txt.find(delimiter, start)) != string::npos) {
+		split = txt.substr(start, found-start);
+		cout << " Split word " << split << endl;
+
+		sub_string_found = split.find(userinput);
+
+		if (sub_string_found != string::npos) {
+			cout << "Found at " << start + sub_string_found << " in " << split << endl;
 		}
+
+		start = found + 1;
+		cnt += 1;
 	}
-	return false;
+	return cnt;
 }
+
+// void subset(string words[], string userinput[]){
+// 	for (int i = 0; i < SIZE2; i++) {
+// 		for (int j = 0; j < SIZE1; j++) {
+// 			if (numbers1[j] != numbers2[i+j]) {
+// 				break;
+// 			}
+
+// 			if (j == SIZE1 - 1)
+// 				return true;
+// 		}
+// 	}
+// 	return false;
+// }
