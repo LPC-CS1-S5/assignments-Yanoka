64712904277bacae89572397243fb2acf7898cb2
Yanoka <>
2021-05-26 05:01:12 +0000

    q4

--- Final/q4.cpp
+++ Final/q4.cpp
@@ -3,30 +3,19 @@
 
 using namespace std;
 
-void subset(string[], string[]);
-
 int splitwords(string txt, char delimiter, string userinput);
 
-const int SIZE1 = 5;
-const int SIZE2 = 10;
-
-int main() {
-	
+int main() {	
 	string  userinput;
   string  words= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quibus natura iure responderit non esse verum aliunde finem beate vivendi, a se principia rei gerendae peti; Quae enim adhuc protulisti, popularia sunt, ego autem a te elegantiora desidero. Duo Reges: constructio interrete. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Bestiarum vero nullum iudicium puto. Nihil enim iam habes, quod ad corpus referas; Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Et homini, qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus?";
 
   cout << "Enter user string "; 
   cin >> userinput;
 
-	splitwords(words, ' ');
-
-	// if (subset(words, userinput))
-	// 	cout << "Subset" << endl;
-	// else
-	// 	cout << "Not subset" << endl;
+	splitwords(words, ' ', userinput);
 }
 
-int splitwords(string txt, char delimiter, string userinput)
+void splitwords(string txt, char delimiter, string userinput)
 {
 	int start=0;
 	int cnt=0;
@@ -37,7 +26,7 @@ int splitwords(string txt, char delimiter, string userinput)
 
 	while ((found = txt.find(delimiter, start)) != string::npos) {
 		split = txt.substr(start, found-start);
-		cout << " Split word " << split << endl;
+		// cout << " Split word " << split << endl;
 
 		sub_string_found = split.find(userinput);
 
@@ -48,19 +37,4 @@ int splitwords(string txt, char delimiter, string userinput)
 		start = found + 1;
 		cnt += 1;
 	}
-	return cnt;
 }
-
-// void subset(string words[], string userinput[]){
-// 	for (int i = 0; i < SIZE2; i++) {
-// 		for (int j = 0; j < SIZE1; j++) {
-// 			if (numbers1[j] != numbers2[i+j]) {
-// 				break;
-// 			}
-
-// 			if (j == SIZE1 - 1)
-// 				return true;
-// 		}
-// 	}
-// 	return false;
-// }
